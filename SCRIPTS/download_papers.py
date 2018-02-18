9bd3fc2d944e01d24ea1dc36ac20b9a9ca991f89
Vivek <vivekbharadwaj.au@gmail.com>
2018-02-18 13:17:04 +1100

    updated nips papers analysis data with 2017 info.

--- SCRIPTS/download_papers.py
+++ SCRIPTS/download_papers.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import json
-import os
+import os, sys
 import pandas as pd
 import re
 import requests
 import subprocess
+from datetime import  datetime, date, timedelta
 
 def text_from_pdf(pdf_path, temp_path):
     if os.path.exists(temp_path):
@@ -27,6 +28,7 @@ nips_authors = set()
 papers = list()
 paper_authors = list()
 
+startime = datetime.now()
 for year in sorted(index_urls.keys()):
     index_url = index_urls[year]
     index_html_path = os.path.join("working", "html", str(year)+".html")
@@ -38,10 +40,10 @@ for year in sorted(index_urls.keys()):
         with open(index_html_path, "wb") as index_html_file:
             index_html_file.write(r.content)
     with open(index_html_path, "rb") as f:
-       html_content = f.read()
+        html_content = f.read()
     soup = BeautifulSoup(html_content, "lxml")
     paper_links = [link for link in soup.find_all('a') if link["href"][:7]=="/paper/"]
-    print("%d Papers Found" % len(paper_links))
+    print("Year: {}; {} Papers Found; Time Elapsed: {} mins".format(year,len(paper_links),str(int((datetime.now()-startime).seconds/60))))
 
 
     temp_path = os.path.join("working", "temp.txt")
@@ -53,7 +55,7 @@ for year in sorted(index_urls.keys()):
         pdf_name = link["href"][7:] + ".pdf"
         pdf_path = os.path.join("working", "pdfs", str(year), pdf_name)
         paper_id = re.findall(r"^(\d+)-", pdf_name)[0]
-        print(year, " ", paper_id) #paper_title.encode('ascii', 'namereplace'))
+#         print(year, " ", paper_id) #paper_title.encode('ascii', 'namereplace'))
         if not os.path.exists(pdf_path):
             pdf = requests.get(pdf_link)
             if not os.path.exists(os.path.dirname(pdf_path)):
@@ -70,12 +72,13 @@ for year in sorted(index_urls.keys()):
             with open(paper_info_html_path, "wb") as f:
                 f.write(r.content)
         with open(paper_info_html_path, "rb") as f:
-           html_content = f.read()
+            html_content = f.read()
         paper_soup = BeautifulSoup(html_content, "lxml")
         try: 
             abstract = paper_soup.find('p', attrs={"class": "abstract"}).contents[0]
         except:
             print("Abstract not found %s" % paper_title.encode("ascii", "replace"))
+            print ("For reference: ",year, " ", paper_id)
             abstract = ""
         authors = [(re.findall(r"-(\d+)$", author.contents[0]["href"])[0],
                     author.contents[0].contents[0])
@@ -97,7 +100,24 @@ for year in sorted(index_urls.keys()):
                 continue
         paper_text = text_from_pdf(pdf_path, temp_path)
         papers.append([paper_id, year, paper_title, event_type, pdf_name, abstract, paper_text])
+        
+    strUntilYear_csv = "2008_to_"+year+".csv"
+    strUntilYear_pickle = "2008_to_"+year+".pickle"
+    pd.DataFrame(list(nips_authors), columns=["id","name"]).sort_values(by="id").to_csv("output/authors"+strUntilYear_csv, index=False)
+    pd.DataFrame(papers, columns=["id", "year", "title", "event_type", "pdf_name", "abstract", "paper_text"]).sort_values(by="id").to_csv("output/papers"+strUntilYear_csv, index=False)
+    pd.DataFrame(paper_authors, columns=["id", "paper_id", "author_id"]).sort_values(by="id").to_csv("output/paper_authors"+strUntilYear_csv, index=False)
+    pd.DataFrame(list(nips_authors), columns=["id","name"]).sort_values(by="id").to_pickle("output/authors"+strUntilYear_pickle)
+    pd.DataFrame(papers, columns=["id", "year", "title", "event_type", "pdf_name", "abstract", "paper_text"]).sort_values(by="id").to_pickle("output/papers"+strUntilYear_pickle)
+    pd.DataFrame(paper_authors, columns=["id", "paper_id", "author_id"]).sort_values(by="id").to_pickle("output/paper_authors"+strUntilYear_pickle)
 
 pd.DataFrame(list(nips_authors), columns=["id","name"]).sort_values(by="id").to_csv("output/authors.csv", index=False)
 pd.DataFrame(papers, columns=["id", "year", "title", "event_type", "pdf_name", "abstract", "paper_text"]).sort_values(by="id").to_csv("output/papers.csv", index=False)
 pd.DataFrame(paper_authors, columns=["id", "paper_id", "author_id"]).sort_values(by="id").to_csv("output/paper_authors.csv", index=False)
+
+# also pickling for easy access
+print ("Remember, these pickles will work only for the following Python version:",sys.version)
+pd.DataFrame(list(nips_authors), columns=["id","name"]).sort_values(by="id").to_pickle("output/authors.pickle")
+pd.DataFrame(papers, columns=["id", "year", "title", "event_type", "pdf_name", "abstract", "paper_text"]).sort_values(by="id").to_pickle("output/papers.pickle")
+pd.DataFrame(paper_authors, columns=["id", "paper_id", "author_id"]).sort_values(by="id").to_pickle("output/paper_authors.pickle")
+
+print("Total Time Elapsed: {} mins".format(str(int((datetime.now()-startime).seconds/60))))
